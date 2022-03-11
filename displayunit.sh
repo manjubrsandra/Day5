3950dc6678ac0cec7d1044eb7c29e4c01013a0b1
manjubrsandra <manjunathbrsandra43@gmail.com>
2022-03-11 09:09:22 +0530

    Added:num_unit

--- displayunit.sh
+++ displayunit.sh
@@ -1,2 +1,23 @@
 #read a number  1,10,100,....and display unit,ten,hundred,...
+read -p "Enter a number: " num
+count=0
+while(( $num != 0 ))
+do
+        num=$(( $num / 10 ))
+        (( count++ ))
+done
+echo "Number of digits: " $count
+if (( $count==1 ))
+then
+        echo "Unit"
+elif (( $count==2 ))
+then
+        echo "Ten"
+elif (( $count==3 ))
+then
+        echo "Hundred"
+elif (( $count==4 ))
+then
+        echo "Thousand"
+fi
 
